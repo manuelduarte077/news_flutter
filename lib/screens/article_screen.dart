f1d4ad1e7d8403894c28373857d79c2ea8e973d4
manuelduarte077 <carlosd99m@gmail.com>
2022-08-06 16:27:10 -0600

    setup routes

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class ArticleScreen extends StatelessWidget {
   const ArticleScreen({Key? key}) : super(key: key);
 
+  static const routeName = '/article';
+
+  static Route route<T>() {
+    return MaterialPageRoute(
+      builder: (_) => const ArticleScreen(),
+      settings: const RouteSettings(name: routeName),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
 
+  static const routeName = '/discover';
+
+  static Route route<T>() {
+    return MaterialPageRoute(
+      builder: (_) => const DiscoverScreen(),
+      settings: const RouteSettings(name: routeName),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
