f934ef0f5c754648447c486b09e8990d4a796198
manuelduarte077 <carlosd99m@gmail.com>
2022-08-06 15:41:17 -0600

    boilerplate initial

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  static const routeName = '/';
+
+  static Route route<T>() {
+    return MaterialPageRoute(
+      builder: (_) => const HomeScreen(),
+      settings: const RouteSettings(name: routeName),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News App'),
+      ),
+    );
+  }
+}
