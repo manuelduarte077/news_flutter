e4a1f9ef9cabb5e0510d94eaad1c7f9f6d4ab0b8
manuelduarte077 <carlosd99m@gmail.com>
2022-08-12 22:37:25 -0600

    create carousel breaking news

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,6 +16,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final article = Article.articles[0];
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -33,30 +35,153 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.zero,
         children: [
-          ImageContainer(
-            height: MediaQuery.of(context).size.height * 0.45,
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            imageUrl: Article.articles[0].imageUrl,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
+          _NewsOfTheDay(article: article),
+          _BreakingNews(articles: Article.articles),
+        ],
+      ),
+    );
+  }
+}
+
+class _BreakingNews extends StatelessWidget {
+  const _BreakingNews({
+    Key? key,
+    required this.articles,
+  }) : super(key: key);
+
+  final List<Article> articles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Breaking News',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'More',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 250,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: articles.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  margin: const EdgeInsets.only(right: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ImageContainer(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        imageUrl: articles[index].imageUrl,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        articles[index].title,
+                        maxLines: 2,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontWeight: FontWeight.bold, height: 1.5),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        '${DateTime.now().difference(articles[index].createAt).inHours} hours ago',
+                        maxLines: 2,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontWeight: FontWeight.bold, height: 1.5),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        'by ${articles[index].author}',
+                        maxLines: 2,
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _NewsOfTheDay extends StatelessWidget {
+  const _NewsOfTheDay({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return ImageContainer(
+      height: MediaQuery.of(context).size.height * 0.45,
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      imageUrl: article.imageUrl,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomTag(
+            backgroundColor: Colors.grey.withAlpha(150),
+            children: [
+              Text(
+                'News of the Day',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Colors.white),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text(
+            article.title,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                fontWeight: FontWeight.bold, height: 1.25, color: Colors.white),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Row(
               children: [
-                CustomTag(
-                  backgroundColor: Colors.grey.withAlpha(150),
-                  children: [
-                    Text(
-                      'News of the Day',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium!
-                          .copyWith(color: Colors.white),
-                    ),
-                  ],
+                Text(
+                  'Learn More',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(color: Colors.white),
+                ),
+                const SizedBox(width: 10),
+                const Icon(
+                  Icons.arrow_right_alt,
+                  color: Colors.white,
                 )
               ],
             ),
-          ),
+          )
         ],
       ),
     );
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,5 +2,3 @@ export 'package:news_app/widgets/custom_tag.dart';
 
 export 'package:news_app/widgets/botton_nav_bar.dart';
 export 'package:news_app/widgets/image_container.dart';
-
-
